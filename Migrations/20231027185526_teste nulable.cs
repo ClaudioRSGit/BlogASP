27f8118fc2a5be4c25edf543cefab2e4ac237e62
Fabio Silva <fabiolazera.lazera@gmail.com>
2023-10-27 21:54:27 +0100

    Regex implemeted

---
+++ Migrations/20231027185526_teste nulable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogASP.Migrations
+{
+    /// <inheritdoc />
+    public partial class testenulable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
