2852ec2bed97c15716393ef74d65a48a09a408bc
Fabio Silva <fabiolazera.lazera@gmail.com>
2023-10-31 11:13:07 +0000

    Redirect with access credentials after sign up created

--- Controllers/SignUpController.cs
+++ Controllers/SignUpController.cs
@@ -1,16 +1,21 @@
 ﻿using BlogASP.Models;
 using BlogASP.Repository;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BlogASP.Controllers
 {
     public class SignUpController : Controller
     {
         private readonly IUserRepository _userRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public SignUpController(IUserRepository userRepository)
+        public SignUpController(IUserRepository userRepository, IHttpContextAccessor httpContextAccessor)
         {
             _userRepository = userRepository;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public IActionResult Index()
@@ -19,9 +24,22 @@ namespace BlogASP.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(UserModel user)
+        public async Task<IActionResult> Create(UserModel user)
         {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties
+            {
+            };
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
             _userRepository.Create(user);
+
             return RedirectToAction("Index", "Home");
         }
     }
